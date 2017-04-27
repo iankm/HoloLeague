78678236384b78b2eddd2f82e6e62a3e751b348a
Melissa <mgree106@jhu.edu>
2017-04-27 11:12:30 -0400

    added skeleton game controller

---
+++ HoloLeagueProject/Assets/Scripts/GameController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    private int redScore;
+    private int blueScore;
+    public int maxScore = 5;
+    private GameObject field;
+    private GameObject ball;
+    private GameObject redCar;
+    private GameObject blueCar;
+
+    // Use this for initialization
+    void Start () {
+        redScore = 0;
+        blueScore = 0;
+        field = GameObject.FindGameObjectWithTag("FieldSystem");
+        redCar = GameObject.FindGameObjectWithTag("TEMPREDCARNAME");
+        blueCar = GameObject.FindGameObjectWithTag("TEMPBLUECARNAME");
+        ball = GameObject.FindGameObjectWithTag("TEMPBALLNAME");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void redGoal() {
+        redScore++;
+        detectGoal();
+    }
+
+    void blueGoal() {
+        blueScore++;
+        detectGoal();
+    }
+
+    void resetPosition() {
+        ComputeBoundary compBoundary = field.GetComponent<ComputeBoundary>();
+        float FLength = compBoundary.fieldLength;
+        float FWidth = compBoundary.fieldWidth;
+        Vector3 CPos = compBoundary.centerPos;
+        ball.transform.position = CPos;
+        redCar.transform.position = (FLength - CPos) / 2;
+        blueCar.transform.position = - (FLength - CPos) / 2;
+    }
+
+    void detectGoal() {
+        if (blueScore == maxScore || redScore == maxScore) {
+            GG();
+        } else {
+            resetPosition();
+        }
+    }
+
+    void GG() {
+        Debug.Log("Game Over");
+        Debug.Log("Red Score: " + redScore);
+        Debug.Log("Blue Score: " + blueScore);
+    }
+
+
+}
